aef7dbb8f21b01f3ed9badca535fa4f7f4f24a17
dorajambor <dorajambor@gmail.com>
2016-05-25 19:06:15 -0400

    removed lists -> activation matrices -> visualize convolutional outputs

--- toy.py
+++ toy.py
@@ -48,10 +48,10 @@ If the input image is of channel 3 (RGB) then your weight vector is n*n*3.
 PARAMETERS you'll need: DEPTH (num of filters), STRIDE (slide filter by), ZERO-PADDING(to control the spatial size of the output volumes). Use (Inputs-FilterSize + 2*Padding)/Stride + 1 to calculate your output volume and to decide your hyperparameters'''
 
 DEPTH = 3
-STRIDE = 2
+STRIDE = 1
 # to ensure that the input and output volumes are the same: use P=(F-1)/2 given stride 1.
 PADDING = 0
-FILTER_SIZE = 5
+FILTER_SIZE = 2
 
 class ToyNet(object):
 
@@ -62,7 +62,7 @@ class ToyNet(object):
         for i in range(DEPTH):
             self.weights.append([np.random.randn(FILTER_SIZE, FILTER_SIZE)])
         self.biases = np.random.rand(DEPTH,1)
-        self.activations = []
+        self.activations = None
 
     def convolve(self, input_neurons):
         '''
@@ -71,35 +71,32 @@ class ToyNet(object):
         output_dim1 = (input_neurons.shape[0] - FILTER_SIZE + 2*PADDING)/STRIDE + 1        # num of rows
         output_dim2 =  (input_neurons.shape[1] - FILTER_SIZE + 2*PADDING)/STRIDE + 1       # num of cols
 
-        for i in range(DEPTH):
-            self.activations.append(np.empty((output_dim1 * output_dim2)))
+        self.activations = np.zeros((DEPTH, output_dim1 * output_dim2))
+
+        # for i in range(DEPTH):
+        #     self.activations.append(np.empty((output_dim1 * output_dim2)))
 
         print 'shape of input (rows,cols): ', input_neurons.shape
         print 'shape of output (rows, cols): ','(', output_dim1,',', output_dim2, ')'
+        act_length =  self.activations[0].shape[0]
+        print 'shape of unrolled convolution output: ', act_length   # one dimensional
 
         for j in range(DEPTH):
             slide = 0
             row = 0
-            print self.activations[j].shape[0]    # one dimensional
 
-            for i in range(self.activations[j].shape[0]):  # loop til the output array is filled up -> one dimensional (600)
+            for i in range(act_length):  # loop til the output array is filled up -> one dimensional (600)
 
                 # ACTIVATIONS -> loop through each 2x2 block horizontally
                 self.activations[j][i] = sigmoid(np.sum(input_neurons[row:FILTER_SIZE+row, slide:FILTER_SIZE + slide] * self.weights[j][0]) + self.biases[j])
                 slide += STRIDE
 
-                if (FILTER_SIZE + slide)-STRIDE >= input_neurons.shape[1]:    # wrap indices at the end of each row
+                if (FILTER_SIZE + slide)-STRIDE >= input_neurons.shape[1]:    # wrap indeces at the end of each row
                     slide = 0
                     row += STRIDE
-
-                if i > 300:
-                    import pdb; pdb.set_trace()
-                    print input_neurons[0]
-                    print input_neurons[row:FILTER_SIZE+row, slide:FILTER_SIZE + slide]
-                    break
-
-            self.activations[j] = self.activations[j].reshape((output_dim1, output_dim2))
-        # print self.activations[0]
+        self.activations = self.activations.reshape((DEPTH, output_dim1, output_dim2))
+        print 'Shape of final conv output: ', self.activations.shape
+        return self.activations
 
 
 class PoolingLayer(object):
@@ -113,18 +110,17 @@ class PoolingLayer(object):
         self.height_in = height_in
         self.depth = depth
         self.poolsize = poolsize
-        self.width_out = (self.width_in - self.poolsize[0])/self.poolsize[1] + 1
+        self.width_out = (self.width_in - self.poolsize[0])/self.poolsize[1] + 1      # num of output neurons
         self.height_out = (self.height_in - self.poolsize[0])/self.poolsize[1] + 1
-        print self.width_out, height_out
+        print 'Pooling shape (row,col): ', self.height_out, self.width_out
 
         # initialize empty output matrix
-        self.output = np.empty((self.depth * self.width_out * self.height_out))
-        self.max_indeces = np.empty((self.depth * self.width_out * self.height_out, 2))
-        print self.output.shape
+        self.output = np.empty((self.depth, self.width_out * self.height_out))
+        self.max_indeces = np.empty((self.depth, self.width_out * self.height_out, 2))
 
     def pool(self, input_image):
         k = 0
-
+        
         # for each filter map
         for j in range(self.depth):
             row = 0
@@ -132,12 +128,12 @@ class PoolingLayer(object):
             for i in range(self.width_out * self.height_out):
                 toPool = input_image[j][row:self.poolsize[0] + row, slide:self.poolsize[0] + slide]
 
-                self.output[k] = np.amax(toPool)                # calculate the max activation
+                self.output[j][k] = np.amax(toPool)                # calculate the max activation
                 index = zip(*np.where(np.max(toPool) == toPool))           # save the index of the max
                 if len(index) > 1:
                     index = [index[0]]
                 index = index[0][0]+ row, index[0][1] + slide
-                self.max_indeces[k] = index
+                self.max_indeces[j][k] = index
 
                 slide += self.poolsize[1]
 
@@ -146,30 +142,61 @@ class PoolingLayer(object):
                     slide = 0
                     row += self.poolsize[1]
                 k += 1
+#                 print 'matrix: ', toPool,'max', self.output[j][k-1]
+#                 print 'index: ', self.max_indeces[j][k-1]
+#                 if k > 10:
+#                     break
 
         self.output = self.output.reshape((self.depth, self.height_out, self.width_out))
         self.max_indeces = self.max_indeces.reshape((self.depth, self.height_out, self.width_out, 2))
-        print 'AFTER RESHPAING:', self.output
-        # print self.max_indeces
+#         print 'AFTER RESHPAING:', self.output
+
 
 class FullyConnectedLayer(object):
-
-    def __init__(self, width_in, height_in, depth, num_neurons):
+    '''
+    Calculates outputs on the fully connected layer then forwardpasses to the final output -> classes
+    '''
+    def __init__(self, depth, height_in, width_in, num_output, num_classes):
         self.width_in = width_in
         self.height_in = height_in
         self.depth = depth
-        self.num_neurons = num_neurons
+        self.num_output = num_output
+        self.num_classes = num_classes
 
-        self.weights = np.random.randn(self.width_in, self.height_in, self.depth, self.width_out, self.height_out)
-        self.biases = np.random.randn(self.width_out, self.height_out)
+        self.weights = np.random.randn(self.num_output, self.depth * self.height_in * self.width_in)
+        self.biases = np.random.randn(self.num_output,1)
+        # self.weights = np.ones((self.num_output, self.depth * self.height_in * self.width_in))
+        # self.biases = np.ones((self.num_output,1))
+        
+        self.output = np.empty((self.num_output))
+        self.final_output = np.empty((self.num_classes))
 
-    def feedforward(self, input_matrix):
-        print self.weights.shape
-        for b,w in zip(self.biases, self.weights):
-            print b.shape,w.shape
-            break 
+    def feedforward(self, a):
+        '''
+        forwardpropagates through the FC layer to the final output layer
+        '''
+        print 'shape of w, input, b: ', self.weights.shape, a.shape, self.biases.shape
+        self.output = sigmoid(np.dot(self.weights, a) + self.biases)
+        print self.output
+        
+        # forwardpass to classification
+        self.final_output = classify(self.output, self.num_output, self.num_classes)
+        return self.final_output
 
 
+# helper functions
+###############################################################
+def classify(x, num_inputs, num_classes):
+    # I. initialize weights and biases!
+    w = np.random.randn(num_classes, num_inputs)
+    b = np.random.randn(num_classes,1)
+    return sigmoid(np.dot(w,x) + b)
+
+def cross_entropy(batch_size, output, expected_output):
+    return (-1/batch_size) * np.sum(expected_output * np.log(output) + (1 - expected_output) * np.log(1-output))
+
+def cross_entropy_prime():
+    return 0
 
 def sigmoid(z):
     return 1.0/(1.0 + np.exp(-z))
@@ -177,14 +204,32 @@ def sigmoid(z):
 def sigmoid_prime(z):
     return sigmoid(z) * (1-sigmoid(z))
 
-
-
+# setting up
+#################################################################
 net = ToyNet([cat.shape[0]*cat.shape[1]])
 print 'yooooo', net.sizes[0]
-net.convolve(cat)
+conv_output = net.convolve(cat)
+print type(conv_output)
+# this is pretty sweet -> see the image after the convolution
+for i in range(conv_output.shape[0]):
+    plt.imsave('cat_conv%s.jpg'%i, conv_output[i])
 
 # TODO: implement for all activations!
-pool_layer = PoolingLayer(net.activations[0].shape[1], net.activations[0].shape[0], len(net.activations)) # only implemented for the first depth layer
-pool_layer.pool(net.activations)
-# fc_layer = FullyConnectedLayer(pool_layer.output.shape[1], pool_layer.output.shape[0], pool_layer.output[0], 10, 1)
-# fc_layer.feedforward(pool_layer.output)
+pool_layer = PoolingLayer(12, 12, 1) # only implemented for the first depth layer
+# # pool_layer.pool(test)
+
+# test = 1 - test /144
+# test = test.reshape((3*12*4,1))
+# fc = FullyConnectedLayer(3,12,4,10,2)
+# fc.feedforward(test)
+
+
+
+# testing
+##################################################################
+# a = np.arange(8).reshape((2*2*2,1))
+# w = np.ones(16).reshape((2,2,2,2))
+# o = np.arange(2).reshape((2,1))
+
+# fc = FullyConnectedLayer(2,2,2,2,0)
+# fc.feedforward(a)
